8ea8c2904ef35b86846560dfd83b3cbaa4a10a8c
Ryan <cadanganryansc@gmail.com>
2024-10-04 11:31:33 +0700

    feat: add hist_equalization.m; fix: tidy lines of code in contrast_stretching.m

--- src/hist_equalization.m
+++ src/hist_equalization.m
@@ -9,37 +9,36 @@ I = imread([nama, '.bmp']);
 figure; imshow(I); title('Citra Masukan');
 hist = make_histogram(I);
 
+freq_list = zeros(C, size(hist,2));
+for k = 1 : size(hist,1)
+    for j = 1 : size(hist,2)
+        if j == 1
+            freq_list(k,j) = hist(k,j) / (M*N);
+        else
+            freq_list(k,j) = freq_list(k,j-1) + (hist(k,j) / (M*N));
+        end
+    end
+end
+
 new_I = zeros(M, N, C);
 
 for k = 1 : C
-    minValue = 0;
-    maxValue = 0;
-
-    for j = 1 : size(hist,2)
-        if hist(k,j) > 0
-            minValue = j-1;
-            break;
-        end
-    end
-
-    for j = size(hist,2) : -1 : 1
-        if hist(k,j) > 0
-            maxValue = j-1;
-            break;
-        end
-    end
-
     for i = 1 : M
         for j = 1 : N
-            new_I(i,j,k) = 255 * (double(I(i,j,k)) - minValue) / (maxValue - minValue);
+            new_I(i,j,k) = round(freq_list(k, I(i,j,k)+1) * 255);
+
+            if new_I(i,j,k) > 255
+                new_I(i,j,k) = 255;
+            elseif new_I(i,j,k) < 0
+                new_I(i,j,k) = 0;
+            end
         end
     end
 end
 
 new_I = uint8(new_I);
-figure; imshow(new_I); title('Citra Kontras Diregangkan')
+figure; imshow(new_I); title('Citra Histogram Diratakan')
 hist = make_histogram(new_I);
-
 
 
 
@@ -63,9 +62,9 @@ function [hist] = make_histogram(I)
         end
     end
 
-    % Membuat histogram
+    % Membuat histogram secara manual
     for k = 1 : C
         figure; disp = bar(0:255, hist(k, :));
-        title(sprintf('Histogram untuk Channel %d', k));
+        title(sprintf('Histogram Manual untuk Channel %d', k));
     end
 end
