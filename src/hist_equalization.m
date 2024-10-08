e6e1e263e4370f0617f90c4d79183dc81b15685a
Ryan <cadanganryansc@gmail.com>
2024-10-08 21:14:31 +0700

    chore: add comments to hist_equalization.m

--- src/hist_equalization.m
+++ src/hist_equalization.m
@@ -3,21 +3,36 @@ function [new_I, change_list] = hist_equalization(I, round)
 
     [M, N, C] = size(I);
     
-    % Menyiapkan change_list untuk untuk semua channel
+    % Menyiapkan change_list dengan 1 baris dan 256 kolom
     change_list = zeros(size(hist,2));
 
+    % ITERASI UNTUK MENGISI CHANGE_LIST
     for j = 1 : size(hist, 2)
         if j == 1
+            % Untuk indeks 1, bagi {rerata semua kolom pada indeks yang 
+            % bersesuaian (j) pada histogram} dengan {ukuran citra}
             change_list(j) = mean(hist(:, j)) / (M*N);
         else
+            % Indeks lainnya, tambahkan nilai pada indeks sebelumnya dengan {rerata 
+            % semua kolom dengan indeks j pada histogram} dibagi {ukuran citra}
             change_list(j) = change_list(j - 1) + mean(hist(:, j)) / (M*N);
+
+            % Mengalikan nilai pada indeks sebelumnya dengan 
+            % nilai piksel maksimum yang mungkin, yaitu 255
             change_list(j - 1) = change_list(j - 1) * 255;
+
+            % MEMBEDAKAN ANTARA HASIL PERKALIAN YANG DI-ROUND DAN TIDAK
+                    % Jika perlu di-round (citra semula), diterapkan fungsi floor 
+                    % ke nilai pada indeks sebelumnya di change_list.
             if round == 1
                 change_list(j - 1) = floor(change_list(j - 1));
             end
-
+            
+            % Menerapkan perkalian dengan 255 untuk nilai pada indeks terakhir
             if j == size(hist, 2)
                 change_list(j) = change_list(j) * 255;
+
+                % Menerapkan fungsi floor (jika round==1) untuk nilai pada indeks terakhir
                 if round == 1
                     change_list(j) = floor(change_list(j));
                 end
